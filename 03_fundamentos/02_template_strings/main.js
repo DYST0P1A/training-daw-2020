f726c25e6589a27dd53ac4b80c315b37afe1c14e
Pablo Gómez <pablogomeztic95@gmail.com>
2020-09-28 16:17:11 +0200

    Ejercicios 2,3,4,5

--- 03_fundamentos/02_template_strings/main.js
+++ 03_fundamentos/02_template_strings/main.js
@@ -1,6 +1,5 @@
-
 function hello(name, lastname, years) {
-
+    return `Hola ${name} ${lastname}, tienes ${years} años de edad`;
 }
 
 let result = hello('John', 'Anderton', 35)
@@ -14,3 +13,5 @@ if (result != "Hola John Foo, tienes 47 años de edad") {
     console.log('test fail')
     return
 }
+
+console.log("todo ok");
--- 03_fundamentos/03_calculator/main.js
+++ 03_fundamentos/03_calculator/main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // Declarar 2 variables con número e imprimir por consola (mediante console.log()) el valor de:
 // Suma
 // Resta
@@ -6,8 +8,12 @@
 
 
 // define variables
-let x, y;
+let x = 65
+let y = 45
 
 
 // print sum, substract, multiply and divide
-console.log('something')
+console.log(`Suma: ${x} + ${y} = ` + (x + y))
+console.log(`Resta: ${x} - ${y} = ` + (x - y))
+console.log(`Multiplicación: ${x} * ${y} = ` + (x * y))
+console.log(`División: ${x} / ${y} = ` + (x / y))
