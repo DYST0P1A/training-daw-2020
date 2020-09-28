1589b4606aa893cf73642fc4a556d85991d4e749
Pablo Gómez <pablogomeztic95@gmail.com>
2020-09-28 16:17:11 +0200

    Ejercicios 2,3,4,5

--- 03_fundamentos/04_person/main.js
+++ 03_fundamentos/04_person/main.js
@@ -1,11 +1,26 @@
+"use strict"
+
 // Crear un objeto Persona con los siguientes 
 // datos (nombre, apellidos, edad, género, dirección)
+
+
+const { kMaxLength } = require('buffer');
 
 const assert = require('assert').strict;
 
 function createPerson(name, lastname, years, gender, address) {
+    let person = {
+        name: 'John',
+        lastname: 'Foo',
+        years: 35,
+        gender: 'male',
+        address: 'fake street 123'
+    }
+    return person
 }
 
-let person = createPerson('John', 'Foo', 35, 'male', 'fake stree 123')
+let person = createPerson('John', 'Foo', 35, 'male', 'fake street 123')
 assert.strictEqual(person.name, 'John')
 assert.strictEqual(person.lastname, 'Foo')
+
+console.log(person)
--- 03_fundamentos/05_math_array/main.js
+++ 03_fundamentos/05_math_array/main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 // Declarar un array numérico (entero) y crear un método que imprima:
 // La suma de todos los elementos
 // El elemento más grande
@@ -5,14 +7,31 @@
 // La media de los elementos
 
 
-
 function doCalculation(array) {
+    let total = 0
+    let vmax = array[0]
+    let vmin = array[0]
 
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+        if (array[i] > vmax) {
+            vmax = array[i]
+        }
+        if (array[i] < vmin) {
+            vmin = array[i]
+        }
+    }
+    console.log("Suma total de los elementos: " + total)
+    console.log("El elemento más grande: " + vmax)
+    console.log("El elemento más pequeño: " + vmin)
 
+    let media = total / array.length
+    console.log("Media de los elementos: " + media)
 }
 
-
-
-doCalculation([1,2,3,4])
-doCalculation([5,5,5,5])
-doCalculation([1,1,1,2,2,2,3,3,3,4,4,4,5,5,5])
+console.log("-Array 1-")
+doCalculation([1, 2, 3, 4])
+console.log("-Array 2-")
+doCalculation([5, 5, 5, 5])
+console.log("-Array 3-")
+doCalculation([1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5])
