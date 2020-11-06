999d3d51fdd691b2ed555a718912ae1ddaa0d9b7
Pablo Gómez <pablogomeztic95@gmail.com>
2020-11-06 16:34:42 +0100

    Ejercicio POO

--- 03_fundamentos/16_ajax/main.js
+++ 03_fundamentos/16_ajax/main.js
@@ -1,13 +1,19 @@
 window.onload = function() {
     console.log('document loaded');
 
-    var xhttp = new XMLHttpRequest();   
+    var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText)
+            // console.log(this.responseText)
+            let obj = JSON.parse(this.response)
+            console.log(obj.status)
+            obj.data.forEach(data => console.log(`${JSON.stringify(data)}`))
+                // obj.data.forEach(function(data) {
+                //     console.log(data)
+                // })
         }
-      };
+    };
 
     xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true);
     xhttp.send();
